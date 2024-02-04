07e31198888bab8e96be3dc6c8f110ff2491c82a
ortizbran001 <ortizbran001@hotmail.com>
2024-02-03 23:42:08 -0600

    generate ID

--- src/App.vue
+++ src/App.vue
@@ -43,10 +43,17 @@
 //handle transaction submitted
 const handleTransactionSubmitted = (transactionData) => {
   transactions.value.push({
+    id: generateUniqueId(),
     text: transactionData.text,
     amount: transactionData.amount,
   })
 }
+
+//generate unique id
+const generateUniqueId = () => {
+  return Math.floor(Math.random() * 10000000)
+}
+
 
 </script>
 
